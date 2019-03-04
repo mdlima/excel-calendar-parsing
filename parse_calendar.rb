ba53d66ce96e6e5424131a60e4490cb384a9dc04
Marcio Lima <mdlima.github@mailnull.com>
2019-03-04 12:32:11 +0100

    Parse multi-day, all-day events

--- parse_calendar.rb
+++ parse_calendar.rb
@@ -29,11 +29,12 @@ def exec(source_file, dest_file)
   class_categories = {}
   calendar_started = false
   # merged_ranges = worksheet.merged_cells.map { |mc| {mc.ref.row_range} }
-  time = nil
   days = {}
   num_rows = 0
 
   worksheet.each do |row|
+    all_day_event = false
+    time = nil
     row = row.cells
     calendar_started ||= Date::MONTHNAMES.compact.map(&:capitalize).include?(row.first.value.to_s.capitalize) if row.first
     unless calendar_started
@@ -58,64 +59,79 @@ def exec(source_file, dest_file)
       next
     end
 
-    if row.first && row.first.number_format && row.first.number_format.format_code == 'h:mm'
-      time = row.first.raw_value.to_f
-      row.compact[1..-1].each do |cell|
-        # next unless cell.value
+    row.compact.each do |cell|
+      if cell.number_format && cell.number_format.format_code == 'h:mm'
+        time = cell.raw_value.to_f
+        next
+      end
 
-        # puts "Parsing event #{cell.value} at cell #{cell.r}"
-        event_text = Loofah.fragment(cell.value).scrub!(:prune).text
-        next if event_text.to_s.strip.empty?
+      next unless cell.value && cell.value.length > 1
 
-        unless days[cell.r.first_col]
-          puts "Cell text without corresponding date at cell #{cell.r}"
-          # exit
+      # puts "Parsing event #{cell.value} at cell #{cell.r}"
+      event_text = Loofah.fragment(cell.value).scrub!(:prune).text
+      next if event_text.to_s.strip.empty?
+
+      unless days[cell.r.first_col]
+        puts "Cell text without corresponding date at cell #{cell.r}"
+        # exit
+        next
+      end
+
+      current_date = days[cell.r.first_col]
+      # Find event end time
+      start_time, end_time = nil
+      event_text_match = event_text.match(/\A(.+)\s+(\d+(?:h|:)\d*)\s?-\s?(\d+(?:h|:)\d*)\s*(.*)/)
+      if event_text_match && event_text_match.captures.length >= 3
+        # Event has time information
+        event_subject = event_text_match.captures.first
+        start_time, end_time = event_text_match.captures[1..2].map { |t| Time.parse(t, current_date) }
+        event_location = event_text_match.captures.length > 3 && !event_text_match.captures.last.to_s.strip.empty? ? event_text_match.captures.last : nil
+      else
+        # puts "Event text doesn't contain time information: event #{event_text} at cell #{cell.r}"
+        # Try to estimate duration based on merged cell ranges
+        cell_range = worksheet.merged_cells.filter { |mc| mc.ref.row_range.include?(cell.r.first_row) && mc.ref.col_range.include?(cell.r.first_col) }
+        if cell_range.empty?
+          puts "Couldn't estimate event time: event #{event_text} at cell #{cell.r}"
           next
         end
 
-        current_date = days[cell.r.first_col]
-        # Find event end time
-        start_time, end_time = nil
-        event_text_match = event_text.match(/\A(.+)\s+(\d+(?:h|:)\d*)\s?-\s?(\d+(?:h|:)\d*)\s*(.*)/)
-        if event_text_match && event_text_match.captures.length >= 3
-          event_subject = event_text_match.captures.first
-          start_time, end_time = event_text_match.captures[1..2].map { |t| Time.parse(t, current_date) }
-          event_location = event_text_match.captures.length > 3 && !event_text_match.captures.last.to_s.strip.empty? ? event_text_match.captures.last : nil
-        else
-          # puts "Event text doesn't contain time information: event #{event_text} at cell #{cell.r}"
-          # Try to estimate duration based on merged cell ranges
-          cell_range = worksheet.merged_cells.filter { |mc| mc.ref.row_range.include?(cell.r.first_row) && mc.ref.col_range.include?(cell.r.first_col) }
-          next if cell_range.empty?
-
-          event_subject = event_text
+        event_subject = event_text
+        if time
+          # Event start time is known
           start_time = current_date + time
           end_time = start_time + cell_range.first.ref.row_range.size * 30.0 / 60.0 / 24.0
           event_location = nil
+        else
+          # Event start time is empty, must be a multi-day, all-day event
+          all_day_event = true
+          start_time = current_date
+          end_time = days[cell.r.first_col + cell_range.last.ref.col_range.size - 1] + 1
         end
-
-        unless start_time
-          puts "Event time not identified: event #{event_text} at cell #{cell.r}"
-          next
-        end
-
-        calendar_events << CalendarEvent.new(
-          subject: event_subject,
-          start_date: start_time,
-          # start_time: start_time,
-          end_date: end_time,
-          # end_time: end_time,
-          all_day_event: false,
-          description: event_text,
-          location: event_location,
-          private: false,
-          class_category: class_categories[cell.fill_color],
-          col_range: cell.r.col_range,
-          row_range: cell.r.row_range,
-          ref: cell.r.to_s,
-          fill_color: cell.fill_color
-        )
       end
+
+      unless start_time
+        puts "Event time not identified: event #{event_text} at cell #{cell.r}"
+        next
+      end
+
+      calendar_events << CalendarEvent.new(
+        subject: event_subject,
+        start_date: start_time,
+        # start_time: start_time,
+        end_date: end_time,
+        # end_time: end_time,
+        all_day_event: all_day_event,
+        description: event_text,
+        location: event_location,
+        private: false,
+        class_category: class_categories[cell.fill_color],
+        col_range: cell.r.col_range,
+        row_range: cell.r.row_range,
+        ref: cell.r.to_s,
+        fill_color: cell.fill_color
+      )
     end
+
     # row_cells = row.map(&:value)
     num_rows += 1
   end
